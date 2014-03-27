28b749604f513363d03a2f2481c48673cbec91d3
piturriky <lluis.eche@gmail.com>
2014-03-27 20:23:18 +0100

    Afegit final

--- games/games/settings.py
+++ games/games/settings.py
@@ -4,6 +4,17 @@ import os.path
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+	"django.contrib.auth.context_processors.auth",
+	"django.core.context_processors.debug",
+	"django.core.context_processors.i18n",
+	"django.core.context_processors.media",
+	"django.core.context_processors.static",
+	"django.core.context_processors.tz",
+	"django.contrib.messages.context_processors.messages",
+	"django.core.context_processors.request",
+)
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
--- games/igames/views.py
+++ games/igames/views.py
@@ -5,13 +5,13 @@ from django.template import Context
 from django.template.loader import get_template
 from django.contrib.auth.models import User
 from igames.models import *
-
 from django.core import serializers
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from sets import Set
+from igames.forms import *
 
 def mainpage(request):
 	template = get_template('mainpage.html')
@@ -24,213 +24,235 @@ def mainpage(request):
 	output = template.render(variables)
 	return HttpResponse(output)
 
-def userpagejson(request,username):
-	data = serializers.serialize("json",User.objects.filter(username=username))
-	return HttpResponse(data, mimetype="application/json")
-
-def userpagexml(request,username):
-	data = serializers.serialize("xml",User.objects.filter(username=username))
-	return HttpResponse(data, mimetype="application/xml")
-
-def userpage(request, username):
+def userpage(request, username, fm):
+	if not request.user.is_authenticated():
+        	return redirect('/login')
 	try:
 		user = User.objects.get(username=username)
-		
 	except:
 		raise Http404('User not found.')
-	games = Game.objects.filter(developers=user)
 
-	platforms = Set([])
-	companies = Set([])
-	for game in games:
-		companies.add(game.company)
-		for platform in game.platforms.all():
-			platforms.add(platform)
+	if not fm:
+		games = Game.objects.filter(developers=user)
+		platforms = Set([])
+		companies = Set([])
+		for game in games:
+			companies.add(game.company)
+			for platform in game.platforms.all():
+				platforms.add(platform)
 	
-	template = get_template('userpage.html')
-	variables = Context({
-		'username': username,
-		'games': games,
-		'platforms': platforms,
-		'companies': companies,
+		template = get_template('userpage.html')
+		variables = Context({
+			'user': user,
+			'games': games,
+			'platforms': platforms,
+			'companies': companies,
 		
-		})
-	output = template.render(variables)
-	return HttpResponse(output)
+			})
+		output = template.render(variables)
+		return HttpResponse(output)
 
-def companiespagejson(request):
-	data = serializers.serialize("json",Company.objects.all())
-	return HttpResponse(data, mimetype="application/json")
+	elif fm == ".xml":
+		data = serializers.serialize("xml",User.objects.filter(username=username))
+		return HttpResponse(data, mimetype="application/xml")
+	elif fm == '.json':
+		data = serializers.serialize("json",User.objects.filter(username=username))
+		return HttpResponse(data, mimetype="application/json")
+	else:
+		raise Http404('Page not found.') 
 
-def companiespagexml(request):
-	data = serializers.serialize("xml",Company.objects.all())
-	return HttpResponse(data, mimetype="application/xml")
+def userspage(request, fm):
+	if not request.user.is_authenticated():
+        	return redirect('/login')
 
-def companiespage(request):
-	template = get_template('generalpage.html')
-	companies = Company.objects.all()
-	variables = Context({
-		'title': 'Companies',
-		'vars': companies,
-		'link': 'companies',
-		})
-	output = template.render(variables)
-	return HttpResponse(output)
+	if not fm:	
+		template = get_template('generalpage.html')
+		developers = User.objects.all()
+		variables = Context({
+			'title': 'Developers',
+			'vars': developers,
+			'link': 'developers',
+			})
+		output = template.render(variables)
+		return HttpResponse(output)
+	elif fm == ".xml":
+		data = serializers.serialize("xml",User.objects.all())
+		return HttpResponse(data, mimetype="application/xml")
+	elif fm == '.json':
+		data = serializers.serialize("json",User.objects.all())
+		return HttpResponse(data, mimetype="application/json")
+	else:
+		raise Http404('Page not found.')
 
-def gamespagejson(request):
-	data = serializers.serialize("json",Game.objects.all())
-	return HttpResponse(data, mimetype="application/json")
+def gamepage(request, gamename,fm):
+	if not request.user.is_authenticated():
+        	return redirect('/login')
+	try:
+		game = Game.objects.get(name=gamename)
+	except:
+		raise Http404('Game not found.')
 
-def gamespagexml(request):
-	data = serializers.serialize("xml",Game.objects.all())
-	return HttpResponse(data, mimetype="application/xml")
+	if not fm:
+		template = get_template('gamepage.html')
+		variables = Context({
+			'game': game,
+			})
+		output = template.render(variables)
+		return HttpResponse(output)
+	elif fm == ".xml":
+		data = serializers.serialize("xml",Game.objects.filter(name=gamename))
+		return HttpResponse(data, mimetype="application/xml")
+	elif fm == '.json':
+		data = serializers.serialize("json",Game.objects.filter(name=gamename))
+		return HttpResponse(data, mimetype="application/json")
+	else:
+		raise Http404('Page not found.')
 
-def gamespage(request):
-	template = get_template('generalpage.html')
-	games = Game.objects.all()
-	variables = Context({
-		'title': 'Games',
-		'vars': games,
-		'link': 'games',
-		})
-	output = template.render(variables)
-	return HttpResponse(output)
+def gamespage(request, fm):
+	if not request.user.is_authenticated():
+        	return redirect('/login')
+	if not fm:
+		template = get_template('generalpage.html')
+		games = Game.objects.all()
+		variables = Context({
+			'title': 'Games',
+			'vars': games,
+			'link': 'games',
+			})
+		output = template.render(variables)
+		return HttpResponse(output)
+	elif fm == ".xml":
+		data = serializers.serialize("xml",Game.objects.all())
+		return HttpResponse(data, mimetype="application/xml")
+	elif fm == '.json':
+		data = serializers.serialize("json",Game.objects.all())
+		return HttpResponse(data, mimetype="application/json")
+	else:
+		raise Http404('Page not found.')
 
-def platformspagejson(request):
-	data = serializers.serialize("json",Platform.objects.all())
-	return HttpResponse(data, mimetype="application/json")
-
-def platformspagexml(request):
-	data = serializers.serialize("xml",Platform.objects.all())
-	return HttpResponse(data, mimetype="application/xml")
-
-def platformspage(request):
-	template = get_template('generalpage.html')
-	platforms = Platform.objects.all()
-	variables = Context({
-		'title': 'Platforms',
-		'vars': platforms,
-		'link': 'platforms',
-		})
-	output = template.render(variables)
-	return HttpResponse(output)
-
-def userspagejson(request):
-	data = serializers.serialize("json",User.objects.all())
-	return HttpResponse(data, mimetype="application/json")
-
-def userspagexml(request):
-	data = serializers.serialize("xml",User.objects.all())
-	return HttpResponse(data, mimetype="application/xml")
-
-def userspage(request):
-	
-	template = get_template('generalpage.html')
-	developers = User.objects.all()
-	variables = Context({
-		'title': 'Developers',
-		'vars': developers,
-		'link': 'developers',
-		})
-	output = template.render(variables)
-	return HttpResponse(output)
-
-
-def companypagejson(request,companyname):
-	data = serializers.serialize("json",Company.objects.filter(name=companyname))
-	return HttpResponse(data, mimetype="application/json")
-
-def companypagexml(request,companyname):
-	data = serializers.serialize("xml",Company.objects.filter(name=companyname))
-	return HttpResponse(data, mimetype="application/xml")
-
-def companypage(request, companyname):
+def companypage(request, companyname, fm):
+	if not request.user.is_authenticated():
+        	return redirect('/login')
 	try:
 		company = Company.objects.get(name=companyname)
 		games = Game.objects.filter(company=company)
 	except:
 		raise Http404('Company not found.')
 
-	developers = Set([])
-	platforms = Set([])
-	for game in games:
-		for platform in game.platforms.all():
-			platforms.add(platform)
-		for developer in game.developers.all():
-			developers.add(developer)
+	if not fm:
+		developers = Set([])
+		platforms = Set([])
+		for game in games:
+			for platform in game.platforms.all():
+				platforms.add(platform)
+			for developer in game.developers.all():
+				developers.add(developer)
 
-	template = get_template('companypage.html')
-	variables = Context({
-		'company': company,
-		'games':games,
-		'platforms': platforms,
-		'developers':developers,
-		})
-	output = template.render(variables)
-	return HttpResponse(output)
+		template = get_template('companypage.html')
+		variables = Context({
+			'company': company,
+			'games':games,
+			'platforms': platforms,
+			'developers':developers,
+			})
+		output = template.render(variables)
+		return HttpResponse(output)
+	elif fm == ".xml":
+		data = serializers.serialize("xml",Company.objects.all())
+		return HttpResponse(data, mimetype="application/xml")
+	elif fm == '.json':
+		data = serializers.serialize("json",Company.objects.all())
+		return HttpResponse(data, mimetype="application/json")
+	else:
+		raise Http404('Page not found.')
 
-def gamepagejson(request,gamename):
-	data = serializers.serialize("json",Game.objects.filter(name=gamename))
-	return HttpResponse(data, mimetype="application/json")
+def companiespage(request, fm):
+	if not request.user.is_authenticated():
+        	return redirect('/login')
+	if not fm:	
+		template = get_template('generalpage.html')
+		companies = Company.objects.all()
+		variables = Context({
+			'title': 'Companies',
+			'vars': companies,
+			'link': 'companies',
+			})
+		output = template.render(variables)
+		return HttpResponse(output)
+	elif fm == ".xml":
+		data = serializers.serialize("xml",Company.objects.all())
+		return HttpResponse(data, mimetype="application/xml")
+	elif fm == '.json':
+		data = serializers.serialize("json",Company.objects.all())
+		return HttpResponse(data, mimetype="application/json")
+	else:
+		raise Http404('Page not found.')
 
-def gamepagexml(request,gamename):
-	data = serializers.serialize("xml",Game.objects.filter(name=gamename))
-	return HttpResponse(data, mimetype="application/xml")
-
-def gamepage(request, gamename):
-	try:
-		game = Game.objects.get(name=gamename)
-	except:
-		raise Http404('Game not found.')
-
-	template = get_template('gamepage.html')
-
-	variables = Context({
-		'game': game,
-		})
-	output = template.render(variables)
-	return HttpResponse(output)
-
-def platformpagejson(request,platformname):
-	data = serializers.serialize("json",Platform.objects.filter(name=platformname))
-	return HttpResponse(data, mimetype="application/json")
-
-def platformpagexml(request,platformname):
-	data = serializers.serialize("xml",Platform.objects.filter(name=platformname))
-	return HttpResponse(data, mimetype="application/xml")
-
-def platformpage(request, platformname):
+def platformpage(request, platformname,fm):
+	if not request.user.is_authenticated():
+        	return redirect('/login')
 	try:
 		platform = Platform.objects.get(name=platformname)
 		games = Game.objects.filter(platforms=platform)
 	except:
 		raise Http404('Platform not found.')
 
-	developers = Set([])
-	companies = Set([])
-	for game in games:
-		companies.add(game.company)
-		for developer in game.developers.all():
-			developers.add(developer)
+	if not fm:
+		developers = Set([])
+		companies = Set([])
+		for game in games:
+			companies.add(game.company)
+			for developer in game.developers.all():
+				developers.add(developer)
 
-	template = get_template('platformpage.html')
-	variables = Context({
-		'platform': platform,
-		'games':games,
-		'companies': companies,
-		'developers':developers,
-		})
-	output = template.render(variables)
-	return HttpResponse(output)
+		template = get_template('platformpage.html')
+		variables = Context({
+			'platform': platform,
+			'games':games,
+			'companies': companies,
+			'developers':developers,
+			})
+		output = template.render(variables)
+		return HttpResponse(output)
+	elif fm == ".xml":
+		data = serializers.serialize("xml",Platform.objects.filter(name=platformname))
+		return HttpResponse(data, mimetype="application/xml")
+	elif fm == '.json':
+		data = serializers.serialize("json",Platform.objects.filter(name=platformname))
+		return HttpResponse(data, mimetype="application/json")
+	else:
+		raise Http404('Page not found.')
+
+def platformspage(request,fm):
+	if not request.user.is_authenticated():
+        	return redirect('/login')
+	if not fm:
+		template = get_template('generalpage.html')
+		platforms = Platform.objects.all()
+		variables = Context({
+			'title': 'Platforms',
+			'vars': platforms,
+			'link': 'platforms',
+			})
+		output = template.render(variables)
+		return HttpResponse(output)
+	elif fm == ".xml":
+		data = serializers.serialize("xml",Platform.objects.all())
+		return HttpResponse(data, mimetype="application/xml")
+	elif fm == '.json':
+		data = serializers.serialize("json",Platform.objects.all())
+		return HttpResponse(data, mimetype="application/json")
+	else:
+		raise Http404('Page not found.')
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserCreateForm(request.POST)
         if form.is_valid():
             new_user = form.save()
             return HttpResponseRedirect("/login")
     else:
-        form = UserCreationForm()
+        form = UserCreateForm()
     return render(request, "registration/register.html", {
         'form': form,
     })
